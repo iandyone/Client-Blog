b0e7a9fdcd6c1e0f0019798a9c3e6095080eb6b4
Andrei Bernatski <bernatski.a@gmail.com>
2023-10-31 19:24:42 +0300

    feat: add banner block

---
+++ src/app/[lang]/privacy/layout.tsx
@@ -0,0 +1,19 @@
+import '@styles/global.scss';
+
+import { IPageLayoutProps } from '@appTypes';
+import { Footer } from '@components/Footer';
+import { Header } from '@components/Header';
+import { getDictionary } from '@utils/dictionaries';
+
+export default async function HomeLayout({ children, params }: IPageLayoutProps) {
+  const { lang } = params;
+  const { header, footer, navigation } = await getDictionary(lang, 'pageLayout');
+
+  return (
+    <>
+      <Header data={header} navigation={navigation} />
+      <main>{children}</main>
+      <Footer data={footer} navigation={navigation} />
+    </>
+  );
+}
