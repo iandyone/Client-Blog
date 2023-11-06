f20c5efbb3db99e3833b59e316b3db95ff938e91
Andrei Bernatski <bernatski.a@gmail.com>
2023-11-06 20:20:43 +0300

    feat: add privacy page tests

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -10,7 +10,7 @@ const BanneComponent: FC<IBannerProps> = ({ data }) => {
   const { text, title, label } = data;
 
   return (
-    <section className={wrapper}>
+    <section className={wrapper} data-testid='banner-component'>
       <div className={container}>
         <Title className={titleClass}>{title}</Title>
         <p className={textClass}>{text}</p>
