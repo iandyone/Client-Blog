ef1900a990495703b918d7ca94d2cbf98646d18b
Andrei Bernatski <bernatski.a@gmail.com>
2023-11-06 22:05:38 +0300

    feat: add category page tests

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -12,9 +12,15 @@ const BanneComponent: FC<IBannerProps> = ({ data }) => {
   return (
     <section className={wrapper} data-testid='banner-component'>
       <div className={container}>
-        <Title className={titleClass}>{title}</Title>
+        <Title className={titleClass} testID='banner-title'>
+          {title}
+        </Title>
         <p className={textClass}>{text}</p>
-        {label && <p className={labelClass}>{label}</p>}
+        {label && (
+          <p className={labelClass} data-testid='banner-label'>
+            {label}
+          </p>
+        )}
       </div>
     </section>
   );
