313884f92dae3863d7a74a4011ef8c1eac310769
Andrei Bernatski <bernatski.a@gmail.com>
2023-11-06 12:02:36 +0300

    feat: add about us video

---
+++ src/components/Header/Modal/index.tsx
@@ -0,0 +1,17 @@
+import { Popup } from '@components/Popup';
+import { FC } from 'react';
+
+const source = process.env.NEXT_PUBLIC_ABOUT_US_VIDEO;
+
+export const Modal: FC = () => {
+  return (
+    <Popup>
+      <iframe
+        width='100%'
+        src={source}
+        title='YouTube video player'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowFullScreen></iframe>
+    </Popup>
+  );
+};
